9f28bbcb2aa21f39e05018c6c6b2804d528918b1
reckjn <julian.reck@gmx.net>
2016-11-29 19:50:32 +0100

    Update download.sh

--- reck-font/download.sh
+++ reck-font/download.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 # You should use curl(1) or wget(1) to download the data automatically
-<<<<<<< HEAD
 git clone https://github.com/ffont/ismir2016
 wget http://labs.freesound.org/static_data/FSL4/FSL4.zip
 # Extract the data using unzip(1) or unrar(1)
 unzip FSL4
-=======
-
-# Extract the data using unzip(1) or unrar(1)
->>>>>>> fa2f7371526d21c17cb2680cd53a30bbf9bf3f55
