f1d8e5237403d437aaefdd946db6c01532fa120a
Arjola <ariolahysneli@hotmail.com>
2016-12-14 11:43:23 +0100

    Update dataset.bat

--- Hysneli-Rafii/dataset.bat
+++ Hysneli-Rafii/dataset.bat
@@ -1,7 +1,7 @@
 @echo off
-rem bitsadmin /create downloadJob
-rem bitsadmin /transfer downloadJob /download /priority normal http://mirlab.org/dataset/public/MIR-1K.rar  C:\Temp\MIR-1K.rar
-rem "C:\Program Files\WinRAR\UnRAR.exe" x C:\Temp\MIR-1K.rar C:\Temp\
+bitsadmin /create downloadJob
+bitsadmin /transfer downloadJob /download /priority normal http://mirlab.org/dataset/public/MIR-1K.rar  C:\Temp\MIR-1K.rar
+"C:\Program Files\WinRAR\UnRAR.exe" x C:\Temp\MIR-1K.rar C:\Temp\
 set mypath=%cd%
 cd %mypath%\dataset
 set mypath=%cd%
