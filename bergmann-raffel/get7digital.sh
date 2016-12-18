21cb4d135997c296ebf3c9444bbed8807138936e
bergmann-fau <michael.bergmann@fau.de>
2016-12-18 08:08:33 +0100

    Delete get7digital.sh

--- bergmann-raffel/get7digital.sh
+++
@@ -1,5 +0,0 @@
-export DIGITAL7_KEY=7dtzvrdqypnw
-export DIGITAL7_SECRET=f9yjz27rmrxj2jrj
-
-#python get7digital.py "Michael Jackson Thriller" midi-dataset/data/mp3/michael_jackson_thriller.mp3
-python get7digital.py
