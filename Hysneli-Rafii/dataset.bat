210e7e9cd150d5e40f4905923a4d8d7f9d9633f1
Arjola <ariolahysneli@hotmail.com>
2016-12-19 11:20:54 +0100

    Delete dataset.bat

--- Hysneli-Rafii/dataset.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-bitsadmin /create downloadJob
-bitsadmin /transfer downloadJob /download /priority normal http://mirlab.org/dataset/public/MIR-1K.rar  C:\Temp\MIR-1K.rar
-"C:\Program Files\WinRAR\UnRAR.exe" x C:\Temp\MIR-1K.rar C:\Temp\
-set mypath=%cd%
-cd %mypath%\dataset
-set mypath=%cd%
-move C:\Temp\MIR-1K\Wavfile %mypath%
-pause
