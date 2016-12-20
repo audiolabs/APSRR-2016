f5087aa1c3ca5bd51da90253fef22c25d39d22fc
robin-nyombi <r.ssenyonga@outlook.com>
2016-12-20 23:24:40 +0300

    update download.sh

--- Ssenyonga-Huang/download.sh
+++ Ssenyonga-Huang/download.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-git clone https://github.com/robin-nyombi/APSRR-2016
-git clone https://github.com/posenhuang/singingvoiceseparationrpca
+git clone git@github.com/robin-nyombi/APSRR-2016.git
+git clone git@github.com/posenhuang/singingvoiceseparationrpca.git
+if [ -f MIR-1K.rar ]
+then
 curl -O http://mirlab.org/dataset/public/MIR-1K.rar
+fi
 unrar x ./MIR-1K.rar
