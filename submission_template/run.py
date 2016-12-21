a914c44070212c73225465332b30301b03ca1b68
robin-nyombi <r.ssenyonga@outlook.com>
2016-12-22 00:41:28 +0300

    Revert "delete files"
    This reverts commit 71959dabe8ce81c58744c28254d58f4b8a70be3f.

---
+++ submission_template/run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# This module should import the library and create the evalualtion
+
+# Alternatively a shell script that invokes several other Python scripts is
+# possible. You should aim for as few commands as possible.
