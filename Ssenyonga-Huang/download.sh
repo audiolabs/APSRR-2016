b6fc440720b02723e4fdea5e0dee462c81a6fb07
robin-nyombi <r.ssenyonga@outlook.com>
2016-12-21 19:56:45 +0300

    update download.sh

--- Ssenyonga-Huang/download.sh
+++ Ssenyonga-Huang/download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone https://github.com/robin-nyombi/APSRR-2016
+cd ..
 git clone https://github.com/posenhuang/singingvoiceseparationrpca
 curl -O http://mirlab.org/dataset/public/MIR-1K.rar
 unrar x ./MIR-1K.rar
